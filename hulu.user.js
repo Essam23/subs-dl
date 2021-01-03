34a8e84e4065754f4de5caa45f5954aa94e2e4c0
subdiox <subdiox@gmail.com>
2021-01-03 22:30:18 +0900

    fix indent

--- hulu.user.js
+++ hulu.user.js
@@ -82,11 +82,11 @@ function downloadDidClick() {
 }
 
 function downloadURI(uri, name) {
-  var link = document.createElement("a");
-  link.download = name;
-  link.href = uri;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  delete link;
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
 }
