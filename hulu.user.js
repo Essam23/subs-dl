0ad5b9506499605da79a2f5f0ee40f551f72074a
Essam23 <87686356+Essam23@users.noreply.github.com>
2021-11-30 19:51:34 +0200

    Update hulu.user.js

--- hulu.user.js
+++ hulu.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Get eab_id
 // @author       subdiox
-// @match        https://www.hulu.com/watch/*
+// @match        https://www.hulu.com/*
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        GM_xmlhttpRequest
